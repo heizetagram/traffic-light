e68a04683e2ba2c1171c77b3e85855bdf6dbc70a
David Lu <davidlu1999@gmail.com>
2023-09-22 15:26:42 +0200

    Minor comment updates

--- src/trafficLight/TrafficLight.java
+++ src/trafficLight/TrafficLight.java
@@ -3,14 +3,17 @@ package trafficLight;
 public class TrafficLight {
     private TrafficLightColor color;
 
+    //Constructor
     public TrafficLight(TrafficLightColor color) {
         setColor(color);
     }
 
+    //Getter
     public TrafficLightColor getColor() {
         return color;
     }
 
+    //Setter
     public void setColor(TrafficLightColor color) {
         this.color = color;
     }
